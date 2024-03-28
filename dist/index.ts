a30cda2af94f56aa55f1e1842662ac8195ec00a0
Zykie Rallos <zykie1018@gmail.com>
2024-03-28 14:41:19 +0800

    Update: file structure and index js in dist directory

--- dist/index.ts
+++
@@ -1,6 +0,0 @@
-import * as Core from "@actions/core";
-import {Importer} from "./Importer";
-
-new Importer().start().then(r => {
-    Core.info(JSON.stringify(r, null, Importer.LOG_SPACING_SIZE))
-})
